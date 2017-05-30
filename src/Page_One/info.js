cc190326afeab1fd691e2e99cdfdc0cd47448765
Mel Rabineau <melanie.rabineau1@gmail.com>
2017-05-30 16:59:23 -0700

    responsive

--- src/Page_One/info.js
+++ src/Page_One/info.js
@@ -6,12 +6,14 @@ class Info extends Component {
     return (
       <div id="info">
       <p id="text">React developer building creative sites for creative people.</p>
-        <h2 id="info">Contact:</h2>
-          <h2 className="contact">Email: Melanie.rabineau1@gmail.com</h2>
-          <h2 className="contact">Github: https://github.com/mrabineau </h2>
-          <h2 className="contact">Instagram: @google_mel </h2>
-          <a href={"https://kimoji.com/password"}><h2 className="contact">Current Favorite Website</h2></a>
+        <div id="me">
+          <h2 id="info">Contact:</h2>
+            <h2 className="contact">Email: Melanie.rabineau1@gmail.com</h2>
+            <h2 className="contact">Github: https://github.com/mrabineau </h2>
+            <h2 className="contact">Instagram: @google_mel </h2>
+            <a href={"https://kimoji.com/password"}><h2 className="contact">Current Favorite Website</h2></a>
         <h1 id="soon">More coming soon</h1>
+        </div>
       </div>
     )
   }
