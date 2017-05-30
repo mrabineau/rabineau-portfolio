a320f52c1d6d30beb8172a9b1e6f645c60a35537
Mel Rabineau <melanie.rabineau1@gmail.com>
2017-05-30 14:40:41 -0700

    scroll instructions added

--- src/Page_One/main.js
+++ src/Page_One/main.js
@@ -4,8 +4,8 @@ import './main.css'
 class Main extends Component {
   render () {
     return (
-      <div>
-        <h1>Hi</h1>
+      <div id="main">
+        <h1 className="intro">COMING SOON</h1>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Main from './Page_One/main.js'
+// import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+  ReactDOM.render((
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={App} />
+      <Route exact path="/main" component ={Main} />
+    </div>
+  </BrowserRouter>
+
+    ), document.getElementById('root'));
+
+
